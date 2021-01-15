c77b667276357b4b55bdb3c6538a3e655ccff67e
Y0unG <lem0n41k@icloud.com>
2021-01-15 23:04:27 +0200

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+
+
+let popup = document.querySelector(".pop-up__container");
+let show = function () {
+    popup.classList.add('pop-up_show');
+}
+let unshow = function () {
+    popup.classList.remove('pop-up_show');
+    console.log(1);
+}
